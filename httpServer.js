8ad82e4abf9fb0363643c7f9c2071d87075dc249
rlyshw <rlyshw@udel.edu>
2015-07-05 00:02:22 -0400

    IT WORKS

--- httpServer.js
+++ httpServer.js
@@ -7,14 +7,32 @@ var server = http.createServer(function(req, res){
 
 var io = require('socket.io')(server);
 
-io.on('connection', function(socket){
-  socket.emit('data', "connection established");
+var everyEvent = 'blur change click dblclick error focus focusin focusout hover keydown keypress keyup load mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup resize scroll select submit';
+
+var jsdom = require("jsdom");
+jsdom.env("./shared.html", ["http://code.jquery.com/jquery.js"],
+function(errors, window){
+  window.$("body").on(everyEvent, function(event){
+    window.$("body").append(event.clientX+" "+event.clientY+"<br>");
+  });
+  io.on('connection', function(socket){
+    socket.emit('data', window.$("body").html());
+    socket.on("event", function(data){
+      if(data.type){
+        window.$("button").trigger(data);
+        io.emit("data", window.$("body").html());
+        return;
+      }
+      window.$("body").html(data);
+      io.emit("data", window.$("body").html());
+    });
+  });
 });
 
 
 
 var port = process.env.PORT || process.argv[2] || 3000;
-var host = process.env.HOST || process.argv[3] || '127.0.0.1';
+var host = process.env.HOST || process.argv[3] || '0.0.0.0';
 server.listen(port, host);
 
 console.log('Server running at http://'+host+":"+port);
--- public/public.js
+++ public/public.js
@@ -1,4 +1,14 @@
 var socket = io();
+
+var everyEvent = 'blur change click dblclick error focus focusin focusout hover keydown keypress keyup load mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup resize scroll select submit';
+
 socket.on("data", function(data){
-  console.log(data);
+  console.log("data received");
+  $("body").html(data);
+});
+
+$("body").on(everyEvent, function(event){
+  event.view = null;
+  console.log(event);
+  socket.emit("event", event);
 });
